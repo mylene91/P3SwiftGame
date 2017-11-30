0e2b8500fc4a532e9fe47209a1e61f528dd01016
Mylène <mylene.greneron@gmail.com>
2017-11-30 22:50:22 +0100

    create "class game" for environment of game

---
+++ P3SwiftGame/P3SwiftGame/Game.swift
@@ -0,0 +1,15 @@
+//
+//  Game.swift
+//  P3SwiftGame
+//
+//  Created by Pro on 30/11/2017.
+//  Copyright © 2017 Mylene. All rights reserved.
+//
+
+import Foundation
+
+// Enter the Game
+
+class Game {
+    
+} // end Game

68c517dcd51390eee16d768571448f339ea6ff80
Mylène <mylene.greneron@gmail.com>
2017-11-30 22:40:49 +0100

    add files : .gitignore for .DS_Store. add Player.swift for create player

---
+++ P3SwiftGame/P3SwiftGame/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  P3SwiftGame
+//
+//  Created by Pro on 30/11/2017.
+//  Copyright © 2017 Mylene. All rights reserved.
+//
+
+import Foundation
+
+
+// create player (totality: 2)
+class Player {
+    
+} // end Player
---
+++ P3SwiftGame/P3SwiftGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  P3SwiftGame
+//
+//  Created by Pro on 30/11/2017.
+//  Copyright © 2017 Mylene. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

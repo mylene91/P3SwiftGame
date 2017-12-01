1439dbf2cb47502123f722d3e1e185a62a26c6f7
Mylène <mylene.greneron@gmail.com>
2017-12-01 15:24:00 +0100

    add initialization Dwarf and function attack

--- P3SwiftGame/P3SwiftGame/Dwarf.swift
+++ P3SwiftGame/P3SwiftGame/Dwarf.swift
@@ -11,4 +11,16 @@ import Foundation
 // Dwarf: fights with an axe
 class Dwarf: Character {
     
+    init(name: String){
+        super.init(name: name, weapon: Axe(), maxlife: 80)
+        self.life = 80
+    }
+    
+    override func attack(target: Character) {
+        let axe = Axe()
+        target.life -= axe.damage
+        print("\(target.name) has lose \(axe.damage) points of life: [\(target.life)/\(target.maxLife)]")
+    } // end attack()
+    
+
 } // end Dwarf

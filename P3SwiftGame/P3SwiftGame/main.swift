5050d002bb94f1f968926c2044fac63a6007bad3
Mylène <mylene.greneron@gmail.com>
2017-12-01 18:03:17 +0100

    test the fighting and heal with the restrictions

--- P3SwiftGame/P3SwiftGame/main.swift
+++ P3SwiftGame/P3SwiftGame/main.swift
@@ -8,5 +8,46 @@
 
 import Foundation
 
-print("Hello, World!")
+var play = Game()
+
+// create six characters
+var unGeant = Giant(name: "GEAAANT")
+var unNain = Dwarf(name: "NAIN")
+var unMage = Magus(name: "MAAGE")
+
+
+var unNain2 = Dwarf(name: "NAIN2")
+var unCombattant2 = Fighter(name: "FIGHHTER2")
+var unMage2 = Magus(name: "MAAGE2")
+
+
+
+// create two teams with six characters
+var teamDeLoosers1: [Character] = [unGeant, unNain, unMage]
+var teamDeLoosers2: [Character] = [unNain2, unCombattant2, unMage2]
+
+
+// two teams presentation
+for characterLoosers in teamDeLoosers1 {
+    characterLoosers.presentation()
+}
+
+
+for characterLoosers in teamDeLoosers2 {
+    characterLoosers.presentation()
+}
+
+
+
+/*
+unMage.heal(target: unMage)
+
+unNain.attack(target: unGeant)
+unNain.attack(target: unMage)
+
+
+unMage.heal(target: unGeant)
+unMage.heal(target: unGeant)
+*/
+
 

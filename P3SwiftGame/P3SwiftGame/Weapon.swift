fd0b8c90c7cf315d5d7983737f18928d859f58c8
Mylène <mylene.greneron@gmail.com>
2017-12-01 14:16:50 +0100

    add main characteristics Weapon

--- P3SwiftGame/P3SwiftGame/Weapon.swift
+++ P3SwiftGame/P3SwiftGame/Weapon.swift
@@ -10,5 +10,12 @@ import Foundation
 
 // weapon for character
 class Weapon {
+    var name: String
+    var damage: Int
+    
+    init (name: String, damage: Int) {
+        self.name = name
+        self.damage = damage
+    }
     
 } // end Weapon

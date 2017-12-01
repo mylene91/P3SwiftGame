e7c599291f75c0f5118dd437f0067ec2ed8b9221
Mylène <mylene.greneron@gmail.com>
2017-12-01 15:27:07 +0100

    add init Magus and func heal + randomHeal (use later)

--- P3SwiftGame/P3SwiftGame/Magus.swift
+++ P3SwiftGame/P3SwiftGame/Magus.swift
@@ -11,4 +11,25 @@ import Foundation
 // Magus is eqquiped with magic wand - No damage, healing power
 class Magus: Character {
     
+    init(name: String){
+        super.init(name: name, weapon: MagicWand(), maxlife: 90)
+    }
+    
+    func heal(target: Character) {
+        //let healTeam: Int = randomHeal()
+        let healTeam = 20
+        target.life += heal
+        
+        if target.life > target.maxLife {
+            target.life = target.maxLife
+            print(target.life)
+        } // end if
+        
+    } // end heal()
+    
+    
+    func randomHeal() -> Int {
+        return Int(arc4random_uniform(UInt32(+15))+5)
+    } // end randomHeal()
+    
 } // end Magus

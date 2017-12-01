86f43ec994937de6adcbb5976cb950154314bcbe
Mylène <mylene.greneron@gmail.com>
2017-12-01 15:28:00 +0100

    add initialization Sword() and function attack (use later)

--- P3SwiftGame/P3SwiftGame/Sword.swift
+++ P3SwiftGame/P3SwiftGame/Sword.swift
@@ -11,4 +11,12 @@ import Foundation
 // sword for Fighter
 class Sword: Weapon {
     
+    init() {
+        super.init(name: "Sword", damage: 150)//attackDamageRandom()) big damages for testing in Game()
+    }
+    
+    // random damage between 10 and 15
+    func attackDamageRandom() -> Int {
+        return Int(arc4random_uniform(UInt32(+6))+10)
+    }
 } // end Sword

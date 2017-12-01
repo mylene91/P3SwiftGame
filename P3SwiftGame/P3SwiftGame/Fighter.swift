c42345ee68a3d1bb99068e2751c7c53f1ef4ab53
Mylène <mylene.greneron@gmail.com>
2017-12-01 15:24:47 +0100

    add initialization Fighter and function attack (use later)

--- P3SwiftGame/P3SwiftGame/Fighter.swift
+++ P3SwiftGame/P3SwiftGame/Fighter.swift
@@ -11,4 +11,15 @@ import Foundation
 // character Fighter
 class Fighter: Character {
     
+    init(name: String){
+        super.init(name: name, weapon: Sword(), maxlife: 100)
+    }
+    
+    
+    override func attack(target: Character) {
+        let attackSword = Sword()
+        target.life -= attackSword.damage
+        print("\(target.name) has lose \(attackSword.damage) points of life: [\(target.life)/\(target.maxLife)]")
+    } // end attack()
+    
 } // end Fighter

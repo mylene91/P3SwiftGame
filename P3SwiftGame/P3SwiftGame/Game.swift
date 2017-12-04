19aa8a3b70e7bc1f3a221b910ce8099276029d97
Mylène <mylene.greneron@gmail.com>
2017-12-04 22:21:52 +0100

    If a Magus is selected, then the player chooses a character from his team to heal, otherwise, the character attacks the adverse team

--- P3SwiftGame/P3SwiftGame/Game.swift
+++ P3SwiftGame/P3SwiftGame/Game.swift
@@ -10,6 +10,7 @@ import Foundation
 
 // Enter the Game
 class Game {
+    //on initialise deux joueurs
     var player1: Player
     var player2: Player
     
@@ -20,10 +21,12 @@ class Game {
     // 1. Team
     func initGame() {
         print("Welcome to the game!")
+        //le player 1 créer son équipe
         print("---------------------------Player 1--------------------------------------")
         print("Create your team Player 1!")
         player1.createTeamCharacters()
         
+        // le joueur 2 créer son équipe
         print("---------------------------Player 2--------------------------------------")
         print("Create your team Player 2!")
         player2.createTeamCharacters()
@@ -31,8 +34,10 @@ class Game {
         print("-------------------------------------------------------------------------")
         print("Well, here are the teams : ")
         print("-------------------------------------------------------------------------")
+        // on affiche l'équipe du joueur 1
                 player1.displayTeam()
         print("-------------------------------------------------------------------------")
+        // on affiche l'équipe du joueur 2
                  player2.displayTeam()
         print("-------------------------------------------------------------------------")
     }
@@ -40,47 +45,74 @@ class Game {
     
     // 2. Fight
     func fight() {
+        // tant que l'équipe du player 2 n'est pas anéantie on fait jouer le player 1 contre le player 2
          while !player2.team.isEmpty {
             print("---------------------------\(player1.namePlayer)--------------------------------------")
+            // le player 1 choisi avec quel personnage attaquer
             let chooseCharacter = player1.selectCharacter(player: player1)
             var index = 0
-            //print(chooseCharacter.name)
+            print(chooseCharacter.name)
             
-            //IMPORTANT AS?
-           if let superMagus = chooseCharacter as? Magus {
-                    print("\(player1.namePlayer) select character to heal in your team")
-                    for characters in player1.team {
-                        print("\(characters.name) - \(characters.life)/\(characters.maxLife) select \(index)")
-                        index += 1
-                        
-                }
-            superMagus.heal(player1.team[Player.answerInt()])
-                  //  magus.heal(player1.team[Player.answerInt()])
+            // s'il a choisi un character de classe mage alors il doit choisir un personnage de son équipe
+                if chooseCharacter is Magus {
+                        print("Bravo ! C'est un mage")
+                        print("\(player1.namePlayer) select character to heal in your team")
+                        for characters in player1.team {
+                            print("\(characters.name) - \(characters.life)/\(characters.maxLife) select \(index)")
+                            index += 1 // à voir car à l'affichage c'est 0 puis 1 puis 2
+                            
+                        }
+                        // le personnage de classe Mage va heal un membre de son équipe selon le choix de l'utilisateur
+                        chooseCharacter.attack(player1.team[Player.answerInt()])
+                    
+                    // si c'est une autre classe que le mage, alors le personnage va choisir un personnage à attaquer dans l'équipe adverse
+                } else {
+                        print("----------------\(player1.namePlayer) attack \(player2.namePlayer)--------------------")
+                        // on affiche l'équipe du player 2
+                        player2.displayTeam()
+                        // le joueur choisi et selon l'entrée de l'utilisateur on assigne - 1 car l'index du tableau commence toujours à 0
+                        index = Player.answerInt() - 1
+                        let theTarget = player2.team[index]
+                    
+                        // on attaque le personnage qui a été choisi dans l'équipe du joueur 2 par le joueur 1
+                        chooseCharacter.attack(theTarget)
+                    
+                        // si le personnage meurt et que sa vie passe en negatif alors sa vie passe à 0
+                        if theTarget.life < 0 {
+                            theTarget.life = 0
+                            // on affiche la team du player 2
+                            print(player2.team)
+                            // on affiche le nom du joueur qui est mort
+                            print("\(theTarget.name) is dead.")
+                            //on affiche son index
+                            print(index)
+                            // .. -1 pour avoir un visu par rapport au tableau qui commence par 0
+                            print(index - 1)
+                            // on enlève ce personnage du tableau avec l'index
+                            player2.team.remove(at: index)
+                            // on affiche l'équipe du joueur 2 sans le personage qui est mort
+                            player2.displayTeam()
+                            print(player2.team)
+                        } // end theTarget
                 
-               
-                
-           } else  {
-            print("----------------\(player1.namePlayer) attack \(player2.namePlayer)--------------------")
-                for characters in player2.team {
-                    print("Joueur adverse = \(characters.name) - \(characters.life)/\(characters.maxLife) tape \(index)")
-                    index += 1
-                }
-                
-               let theTarget = player2.team[Player.answerInt()]
-               chooseCharacter.attack(target: theTarget)
-            
-                if theTarget.life == 0 { // test if character is dead display "DEAD" -> idea: func check if dead
-                print("DEAD")
-                
-            } // end else
+                } // end else
         } // end while
-
-   // } // end fight()
-    
-    
-    
+    } // end fight()
 } // end Game()
 
+            
+            //IMPORTANT AS?
+//           if let superMagus = chooseCharacter as? Magus {
+//                    print("\(player1.namePlayer) select character to heal in your team")
+//                    for characters in player1.team {
+//                        print("\(characters.name) - \(characters.life)/\(characters.maxLife) select \(index)")
+//                        index += 1
+//
+//                }
+//
+//           superMagus.heal(player1.team[Player.answerInt()])
 
-}
-}
+
+        
+
+

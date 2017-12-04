13258c8fd82c063f929883de71699920653f3ce9
Mylène <mylene.greneron@gmail.com>
2017-12-04 12:05:47 +0100

    add static func answerInt and func selectCharacter

--- P3SwiftGame/P3SwiftGame/Player.swift
+++ P3SwiftGame/P3SwiftGame/Player.swift
@@ -11,50 +11,65 @@ import Foundation
 
 // create player (totality: 2)
 class Player {
-    // 1. Player creates team with three characters
-    var teamCharacters = [Character]()
+    // 1. The player creates a team of 3 characters
+    var team = [Character]()
+    var namePlayer: String
     
+    init(nameplayer: String) {
+        self.namePlayer = nameplayer
+    }
     
+    // GENERAL
+    static func answerInt() -> Int {
+        if let answer = readLine() {
+            if let reponseAnswer = Int(answer) {
+                return reponseAnswer
+            }
+        }
+        return answerInt()
+    } // end answerInt()
+    
+    // 1. Create a team of 3 characters (add array)
     func createTeamCharacters() {
-        while teamCharacters.count < 3 {
+        while team.count < 3 {
             print("Choose character :"
                 + "\n1. Fighter"
                 + "\n2. Magus"
                 + "\n3. Giant"
                 + "\n4. Dwarf")
             
-            if let choiceClass = readLine() {
+            if let chooseClass = readLine() {
                 var character: Character
                 
-                switch choiceClass {
+                switch chooseClass {
                 case "1":
                     print("You have selected a Fighter" + "\n")
                     let name = nameCharacter()
                     character = Fighter(name: name)
-                    teamCharacters.append(character)
+                    team.append(character)
                 case "2":
                     print("You have selected a Magus" + "\n")
                     let name = nameCharacter()
                     character = Magus(name: name)
-                    teamCharacters.append(character)
+                    team.append(character)
                 case "3":
                     print("You have selected a Giant" + "\n")
                     let name = nameCharacter()
                     character = Giant(name: name)
-                    teamCharacters.append(character)
+                    team.append(character)
                 case "4":
-                    print("You have selected a Dward" + "\n")
+                    print("You have selected a Dwarf" + "\n")
                     let name = nameCharacter()
                     character = Dwarf(name: name)
-                    teamCharacters.append(character)
+                    team.append(character)
                 default:
-                    print("I don't understand, try again please.")
+                    print("I don't understand, please try again.")
                 } // end switch
             } //end if
         } // end while count
     } // end createTeamCharacters()
     
-    // 1. Player: choose his name (->string)
+    // 1. Player: Choose his name (->string)
     func nameCharacter() -> String {
         var validName = true
         var chooseName = String()
@@ -68,11 +83,11 @@ class Player {
                     chooseName = name
                 } else {
                     validName = false
-                    print("I don't understand, try again please." + "\n")
+                    print("I don't understand, please try again." + "\n")
                 }
             } else {
                 validName = false
-                print("I don't understand, try again please." + "\n")
+                print("I don't understand, please try again." + "\n")
             }
         } while !validName
         return chooseName
@@ -80,10 +95,32 @@ class Player {
     
     // 1. Presentation Team
     func displayTeam() {
-        for character in teamCharacters {
+        for character in team {
             character.presentation()
         }
     } // end displayTeam()
     
+    // 2. Select a character in order to fight the adverse team
+    func selectCharacter(player: Player) -> Character {
+        let characterToFight = Character()
+        
+        displayTeam()
+        let userChoice = Player.answerInt()
+        switch userChoice {
+        case 1:
+            print("You have chosen \(player.team[0].name)-\(player.team[0].typeClass) - [\(player.team[0].life)/\(player.team[0].maxLife)]!")
+            
+        case 2:
+            print("You have chosen \(player.team[1].name) - \(player.team[1].typeClass)- [\(player.team[1].life)/\(player.team[1].maxLife)]!")
+            
+        case 3:
+            print("You have chosen \(player.team[2].name) - \(player.team[0].typeClass) - [\(player.team[2].life)/\(player.team[2].maxLife)]!")
+
+        default:
+            print("I don't understand, please try again.")
+        }
+        return characterToFight
+        
+    }
     
 } // end Player

486dc14e87bc05a35bf7c4ed02576b7c6278889d
Mylène <mylene.greneron@gmail.com>
2017-12-04 22:13:00 +0100

    news comments

--- P3SwiftGame/P3SwiftGame/Player.swift
+++ P3SwiftGame/P3SwiftGame/Player.swift
@@ -15,11 +15,12 @@ class Player {
     var team = [Character]()
     var namePlayer: String
     
+    //initialisation de nameplayer
     init(nameplayer: String) {
         self.namePlayer = nameplayer
     }
     
-    // GENERAL
+    //fonction pour que l'utilisateur renvoie uniquement un nombre entier
     static func answerInt() -> Int {
         if let answer = readLine() {
             if let reponseAnswer = Int(answer) {
@@ -31,6 +32,7 @@ class Player {
     
     // 1. Create a team of 3 characters (add array)
     func createTeamCharacters() {
+        //tant que la team ne compte pas 3 personnages propose au joueur de 1. selectionner un personnage 2. le nommer
         while team.count < 3 {
             print("Choose character :"
                 + "\n1. Fighter"
@@ -46,21 +48,25 @@ class Player {
                     print("You have selected a Fighter" + "\n")
                     let name = nameCharacter()
                     character = Fighter(name: name)
+                    //ajoute au tableau team ton character
                     team.append(character)
                 case "2":
                     print("You have selected a Magus" + "\n")
                     let name = nameCharacter()
                     character = Magus(name: name)
+                    //ajoute au tableau team ton character
                     team.append(character)
                 case "3":
                     print("You have selected a Giant" + "\n")
                     let name = nameCharacter()
                     character = Giant(name: name)
+                    //ajoute au tableau team ton character
                     team.append(character)
                 case "4":
                     print("You have selected a Dwarf" + "\n")
                     let name = nameCharacter()
                     character = Dwarf(name: name)
+                    //ajoute au tableau team ton character
                     team.append(character)
                 default:
                     print("I don't understand, please try again.")
@@ -69,18 +75,23 @@ class Player {
         } // end while count
     } // end createTeamCharacters()
     
-    // 1. Player: Choose his name (->string)
+    // 1. Player: Choose his name (->string) : choisi un nom pour le personnage que tu viens de selectionner et renvoie uniquement un string
     func nameCharacter() -> String {
         var validName = true
         var chooseName = String()
         
+        //jusqu'à ce que la condition soit bonne fait ceci et retourne chooseName
         repeat{
             print("NAME IT :")
+            // demande au joueur de donner un nom à son personnage qu'il vient de créer
             if let name = readLine() {
+                //si le nom contient bien un string alors affiche le dans un print et le booleen est true, choosename sera égal de nature String
                 if name != "" {
+                    //print le nom qu'il vient de choisir et il est
                     print("Name:  \(name)." + "\n")
                     validName = true
                     chooseName = name
+                // sinon le validName est faux
                 } else {
                     validName = false
                     print("I don't understand, please try again." + "\n")
@@ -89,6 +100,7 @@ class Player {
                 validName = false
                 print("I don't understand, please try again." + "\n")
             }
+        //stop la boucle
         } while !validName
         return chooseName
     } // end nameCharacter()
@@ -100,25 +112,33 @@ class Player {
         }
     } // end displayTeam()
     
+    
     // 2. Select a character in order to fight the adverse team
     func selectCharacter(player: Player) -> Character {
-        let characterToFight = Character()
+        // creation d'un container pour stocker le personnage
+        var characterToFight = Character(typeclass: "", name: "", weapon: Axe(), maxlife: 0)
         
-        displayTeam()
+        //montrer les équipes pour faire un choix
+         displayTeam()
+        //le joueur rentre une valeur
         let userChoice = Player.answerInt()
+        //recup valeur dans le switch on l'assigne pour l'équipe adverse
         switch userChoice {
         case 1:
+            // j'assigne le character de l'équipe dans le container
+            characterToFight = player.team[0]
             print("You have chosen \(player.team[0].name)-\(player.team[0].typeClass) - [\(player.team[0].life)/\(player.team[0].maxLife)]!")
-            
         case 2:
+            characterToFight = player.team[1]
             print("You have chosen \(player.team[1].name) - \(player.team[1].typeClass)- [\(player.team[1].life)/\(player.team[1].maxLife)]!")
-            
         case 3:
+            characterToFight = player.team[2]
             print("You have chosen \(player.team[2].name) - \(player.team[0].typeClass) - [\(player.team[2].life)/\(player.team[2].maxLife)]!")
-
+            
         default:
             print("I don't understand, please try again.")
         }
+        // je renvoie le container(contenant le joueur selectionné)
         return characterToFight
         
     }

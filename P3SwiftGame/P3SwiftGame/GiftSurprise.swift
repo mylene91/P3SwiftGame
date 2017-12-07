7e38644d847de011772555b10447a553a5dc6571
Mylène <mylene.greneron@gmail.com>
2017-12-07 13:42:12 +0100

    file added : GiftSurprise : contains a random weapon

---
+++ P3SwiftGame/P3SwiftGame/GiftSurprise.swift
@@ -0,0 +1,29 @@
+//
+//  GiftSurprise.swift
+//  P3SwiftGame
+//
+//  Created by Pro on 06/12/2017.
+//  Copyright © 2017 Mylene. All rights reserved.
+//
+
+import Foundation
+
+class GiftSurprise: Weapon {
+    
+    init() {
+        super.init(name: randomName(), damage: damageRandom())
+    }
+    
+    //random damage between 20 and 25
+    func damageRandom() -> Int {
+        return Int(arc4random_uniform(UInt32(+6))+22)
+    }
+    
+    //radom de noms
+    func randomName() -> String {
+        let arrayNameRandom = ["Candle", "Saussage", "Cupcake", "Beer"]
+        let newNameRandom = arrayNameRandom[Int(arc4random_uniform(UInt32(arrayNameRandom.count)))]
+        return newNameRandom
+    }
+    
+}
